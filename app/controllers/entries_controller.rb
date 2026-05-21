f96606db6fa6488d27fdb1fd71fd0deccbd0577c
Kenneth Lee <kennethlee2026@u.northwestern.edu>
2026-05-21 03:41:48 +0000

    update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,12 +5,11 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new(
-      title: params[:entry][:title],
-      description: params[:entry][:description],
-      occurred_on: params[:entry][:occurred_on],
-      place_id: params[:entry][:place_id]
-    )
+    @entry = Entry.new
+    @entry.title = params[:entry][:title]
+    @entry.description = params[:entry][:description]
+    @entry.occurred_on = params[:entry][:occurred_on]
+    @entry.place_id = params[:entry][:place_id]
     @entry.save
     redirect_to place_path(@entry.place)
   end
