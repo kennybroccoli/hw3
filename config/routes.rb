de94ec348fc4bb4d5bbed903363804719fb50c87
Kenneth Lee <kennethlee2026@u.northwestern.edu>
2026-05-21 03:34:52 +0000

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get "entries/new"
-  get "entries/create"
-  get "places/index"
-  get "places/show"
-  get "places/new"
-  get "places/create"
   resources :places
   resources :entries
 
